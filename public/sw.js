9834aeb08c65936813d3b9cc856bd155ae1f70e3
Kasireddy Dinitha <kasireddydinithak@gmail.com>
2026-09-08 21:43:53 +0530

    ccccccccccc

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "veda-web-v2";
+const CACHE_NAME = "veda-web-v3";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(Promise.all([
